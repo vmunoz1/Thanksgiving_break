af7baaf0781670e48b7a1b2da20ab71181cb9992
vmunoz1 <45017690+vmunoz1@users.noreply.github.com>
2019-12-02 10:27:03 -0800

    Add files via upload

---
+++ thanksgiving.js
@@ -0,0 +1,15 @@
+var a = document.getElementById("1pic")
+var b = document.getElementById("pup1")
+var c = document.getElementById("pup2")
+var d = document.getElementById("pup4")
+var e = document.getElementById("pup5")
+
+function changePup(elem){
+	b.src="Images/pup2.jpg";
+	a.classList.add("opaque");
+}
+
+function factOne(elem){
+	d.classList.add("opaque");
+	
+}
